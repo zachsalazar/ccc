86e08c26ce1338f0f99bd7d4f43b6d33c9645f7b
Josiah Pohl <josiahpohl@live.com>
2021-03-02 10:47:40 -0500

    add book tab completion

---
+++ completion.bash
@@ -0,0 +1,28 @@
+_kjv_comp() {
+    COMPREPLY=( $(compgen -W "$1" -- ${word}) )
+    if [[ ${#COMPREPLY[@]} == 1 && ${COMPREPLY[0]} == "--"*"=" ]]; then
+        # If there's only one option, with =, then discard space
+        complete -o nospace
+    fi
+}
+
+_kjv() {
+    COMPREPLY=()
+    complete +o default # Disable default to not deny completion, see: http://stackoverflow.com/a/19062943/1216348
+
+    local word="${COMP_WORDS[COMP_CWORD]}"
+    local prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    case "${COMP_CWORD}" in
+        1)
+            if [[ $word == -* ]]; then
+                _kjv_comp '-l -W -h'
+            else
+                kjv -l | while read b;
+                    do echo ${b% *}
+                done
+            fi
+            ;;
+    esac
+}
+complete -F _kjv kjv
